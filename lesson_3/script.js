f6beb088217ffe65a3ae71ba01c6605c636023c3
user <user@lab2095m.tumo.lab>
2019-06-04 20:40:34 +0400

    new folder

---
+++ lesson_3/script.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+   res.send("Hello world");
+});
+
+app.get("/google", function(req, res){
+	res.redirect('http://google.com')
+ });
+
+app.get("/google/:search", function(req, res){
+	var value = req.params.search;
+	res.redirect('http://google.com/search?q='+value)
+ });
+
+
+
+app.listen(3000, function(){
+   console.log("Example is running on port 3000");
+});
