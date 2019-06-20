1a7de86164d53572dc0a4ed24747ce1565b77cc1
Home <Home@LAPTOP-N601TTK4>
2019-06-21 00:29:25 +0400

    w
    work

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,10 +1,90 @@
-var express = require("express");
-var app = express();
 
-app.get("/", function(req, res){
-   res.send("Hello");
-});
+function setup() {
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
-});
+   var socket = io();
+
+   var side = 15;
+
+   var matrix = [];
+
+   // let grassCountElement = document.getElementById('grassCount');
+   // let grassEaterCountElement = document.getElementById('grassEaterCount');
+   let weatherImg = document.getElementById("weatherImg");
+   let weatherTxt = document.getElementById("weatherTxt");
+   socket.on("data", drawCreatures);
+
+   function drawCreatures(data) {
+      let wetIndent;
+      //give matrix from server
+      matrix = data.matrix;
+      //document.write(data.weather);
+
+      //CreatImage("./images/summer.png",100,100);
+      if (data.weather == "summer") {
+         weatherImg.src = "./images/summer.png";
+         weatherTxt.innerText = "Summer";
+         wetIndent = 1;
+      }
+      else if(data.weather == "autumn"){
+         weatherImg.src = "./images/autumn.png";
+         weatherTxt.innerText = "Autumn";
+         wetIndent = 2;
+      }
+      else if(data.weather == "winter"){
+         weatherImg.src = "./images/winter.png";
+         weatherTxt.innerText = "Winter";
+         wetIndent = 3;
+      }
+      else if(data.weather == "spring"){
+         weatherImg.src = "./images/spring.png";
+         weatherTxt.innerText = "Spring";
+         wetIndent = 4;
+      }
+
+      //grassCountElement.innerText = data.grassCounter;
+
+      createCanvas(matrix[0].length * side, matrix.length * side);
+      background('#acacac');
+
+      for (var i = 0; i < matrix.length; i++) {
+         for (var j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == 1) {
+               fill("green");
+               rect(j * side, i * side, side, side);
+            } else if (matrix[i][j] == 2) {
+               let col;
+               if(wetIndent == 3){
+                  col = color(193, 136, 3); 
+               }
+               else{
+                  col = color(235, 255, 25); 
+               }
+               fill(col);
+               rect(j * side, i * side, side, side);
+            } 
+            else if (matrix[i][j] == 0) {
+               fill('#acacac');
+               rect(j * side, i * side, side, side);
+            }
+            // } else if (matrix[i][j] == 3) {
+            //    fill('red');
+            //    rect(j * side, i * side, side, side);
+            // } else if (matrix[i][j] == 4) {
+            //    fill('blue');
+            //    rect(j * side, i * side, side, side);
+            // } else if (matrix[i][j] == 5) {
+            //    fill('yellow');
+            //    rect(j * side, i * side, side, side);
+            // }
+         }
+      }
+   }
+}
+
+function ChangeImage(src) {
+   var img = document.createElement("img");
+   img.src = src;
+
+
+   document.body.appendChild(img);
+}
--- Final_Project/server.js
+++ Final_Project/server.js
@@ -1,10 +1,120 @@
-var express = require("express");
+//Node
+var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.get("/", function(req, res){
-   res.send("Server");
+//use html
+app.use(express.static("."));
+app.get('/', function (req, res) {
+   res.redirect('index.html');
 });
 
-app.listen(3000, function(){
-   console.log("Example is running on port 3000");
+//set localhost
+server.listen(3000, function () {
+   console.log("Program is running on port 3000 To access write 'http://localhost:3000/'");
 });
+
+//Classes
+var Grass = require("./modules/Grass.js");
+var GrassEater = require("./modules/GrassEater.js");
+
+let random = require('./modules/random');
+
+matrix = [];
+
+//Arrays
+grassArr = [];
+grassEaterArr = [];
+
+
+//Global Variables
+grassHashiv = 0;
+weather = "summer";
+
+
+
+
+function matrixGenerator(matrixSize, grass, grassEater) {
+   for (let i = 0; i < matrixSize; i++) {
+      matrix[i] = [];
+      for (let o = 0; o < matrixSize; o++) {
+         matrix[i][o] = 0;
+      }
+   }
+   for (let i = 0; i < grass; i++) {
+      let customX = Math.floor(random(matrixSize));
+      let customY = Math.floor(random(matrixSize));
+      matrix[customY][customX] = 1;
+   }
+   for (let i = 0; i < grassEater; i++) {
+      let customX = Math.floor(random(matrixSize));
+      let customY = Math.floor(random(matrixSize));
+      matrix[customY][customX] = 2;
+   }
+}
+
+matrixGenerator(50, 10, 7);
+
+function creatingObjects() {
+   for (var y = 0; y < matrix.length; y++) {
+      for (var x = 0; x < matrix[y].length; x++) {
+         if (matrix[y][x] == 2) {
+            var grassEater = new GrassEater(x, y);
+            grassEaterArr.push(grassEater);
+         }
+         else if (matrix[y][x] == 1) {
+            var grass = new Grass(x, y);
+            grassArr.push(grass);
+            grassHashiv++;
+         }
+      }
+   }
+}
+creatingObjects();
+
+
+var wCount = 0;
+function game() {
+   if (grassArr[0] !== undefined) {
+      for (var i in grassArr) {
+         grassArr[i].mul();
+      }
+   }
+   if (grassEaterArr[0] !== undefined) {
+      for (var i in grassEaterArr) {
+         grassEaterArr[i].eat();
+      }
+   }
+   wCount++;
+
+   if (wCount<=10) {
+      weather = "summer";
+   }
+   else if(wCount<=20){
+      weather = "autumn";
+   }
+   else if(wCount<=30){
+      weather = "winter";
+   }
+   else if(wCount<40){
+      weather = "spring";
+   }
+   else{
+      wCount = 0;
+   }
+
+
+   let sendData = {
+      matrix: matrix,
+      grassCounter: grassHashiv,
+      weather: weather
+   }
+
+
+   io.sockets.emit("data", sendData);
+}
+
+
+
+setInterval(game, 1000);
